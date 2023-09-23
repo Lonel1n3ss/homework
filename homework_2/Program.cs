5a153b534b042ac1e6e354e2f8368ae351dc8e4c
Loneliness <Strelok20152@gmail.com>
2023-09-23 22:40:41 +0300

    test 4

---
+++ homework_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число а");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b){
+    Console.Write($"Наибольшее число - {a}");
+}
+else {
+    Console.Write($"Наибольшее число -{b}");
+}
